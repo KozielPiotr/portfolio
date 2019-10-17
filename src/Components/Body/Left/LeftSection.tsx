46c249b7140bf71bc13fc7a1f99ef34f76297535
KozielPiotr <koziel.piotr1988@gmail.com>
2019-10-17 23:49:05 +0200

    Deleted unused import

--- src/Components/Body/Left/LeftSection.tsx
+++ src/Components/Body/Left/LeftSection.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import {Grid, Paper, makeStyles, Theme, createStyles, Container, Avatar} from "@material-ui/core";
+import {Grid, Paper, makeStyles, Theme, createStyles, Container} from "@material-ui/core";
 
 
 const useStyles = makeStyles((theme: Theme) =>
