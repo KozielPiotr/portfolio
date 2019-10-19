11800e2f19384b2e6a03ea6a8050e1fbf0e87e42
KozielPiotr <koziel.piotr1988@gmail.com>
2019-10-19 11:32:26 +0200

    formatted contact section

--- src/Components/Body/Left/Contact.tsx
+++ src/Components/Body/Left/Contact.tsx
@@ -1,6 +1,6 @@
-import React from "react"
+import React, { Fragment } from "react"
 
-import {List, ListItem, ListSubheader, makeStyles, Theme, createStyles, Typography, Container} from "@material-ui/core"
+import {List, ListItem, ListSubheader, makeStyles, Theme, createStyles, Typography, Link} from "@material-ui/core"
 
 import { personal_info } from '../../personal_data_storel';
 
@@ -9,13 +9,35 @@ const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
             padding: 0,
+        },
+        mainItem: {
+            paddingTop: 0,
+            paddingBottom: 0,
+        },
+        nested: {
+            padding: theme.spacing(0, 0, 0, 4),
         }
     }),
 );
 
 
+const isEmail = (title: string, description: string) => {
+    if (title==="Email:") {
+        return (
+            <Link href={"mailto:" + description} color={"inherit"} underline={"always"}>
+                {description}
+            </Link>
+        );
+    };
+    return (
+        description
+    );
+}
+
+
 export default () => {
     const classes = useStyles();
+    
     return (
         <div className={classes.root}>
             <List
@@ -28,18 +50,20 @@ export default () => {
                 className={""}
             >
                 {personal_info.map((data) => 
-                    <ListItem>
-                        <div>
+                    <Fragment>
+                        <ListItem className={classes.mainItem}>
                             <Typography variant={"h6"}>
                                 {data.title}
                             </Typography>
-                        </div>
-                        <div>
-                            <Typography variant={"subtitle1"}>
-                                <br/>{data.description}
-                            </Typography>
-                        </div>
-                    </ListItem>
+                        </ListItem>
+                        <List component="div" disablePadding>
+                            <ListItem className={classes.nested}>
+                                <Typography variant={"subtitle1"}>
+                                    {isEmail(data.title, data.description)}
+                                </Typography>
+                            </ListItem>
+                        </List>
+                    </Fragment>
                 )}
             </List>
         </div>
