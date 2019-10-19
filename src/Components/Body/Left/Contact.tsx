e4cf936d918cd757077483ecda53ab113eaa4fc0
KozielPiotr <koziel.piotr1988@gmail.com>
2019-10-19 12:05:01 +0200

    contact section is now collapsed and can be uncovered

--- src/Components/Body/Left/Contact.tsx
+++ src/Components/Body/Left/Contact.tsx
@@ -1,8 +1,9 @@
 import React, { Fragment } from "react"
 
-import {List, ListItem, ListSubheader, makeStyles, Theme, createStyles, Typography, Link} from "@material-ui/core"
+import {List, ListItem, ListSubheader, makeStyles, Theme, createStyles, Typography, Link, Collapse} from "@material-ui/core"
+import {ExpandLess, ExpandMore} from "@material-ui/icons";
 
-import { personal_info } from '../../personal_data_storel';
+import { personal_info } from "../../personal_data_storel";
 
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -37,32 +38,40 @@ const isEmail = (title: string, description: string) => {
 
 export default () => {
     const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+
+    const handleClick = () => {
+        setOpen(!open);
+    };
     
     return (
         <div className={classes.root}>
             <List
                 component="nav"
                 subheader={
-                    <ListSubheader component="div" id="nested-list-subheader">
-                        Contact:
+                    <ListSubheader component="div" id="nested-list-subheader" onClick={handleClick}>
+                        Contact: {open ? <ExpandLess /> : <ExpandMore />}
                     </ListSubheader>
                 }
                 className={""}
-            >
+            >   
                 {personal_info.map((data) => 
                     <Fragment>
-                        <ListItem className={classes.mainItem}>
-                            <Typography variant={"h6"}>
-                                {data.title}
-                            </Typography>
-                        </ListItem>
-                        <List component="div" disablePadding>
-                            <ListItem className={classes.nested}>
-                                <Typography variant={"subtitle1"}>
-                                    {isEmail(data.title, data.description)}
+                        
+                        <Collapse in={open} timeout="auto" unmountOnExit>
+                            <ListItem className={classes.mainItem}>
+                                <Typography variant={"h6"}>
+                                    {data.title}
                                 </Typography>
                             </ListItem>
-                        </List>
+                            <List component="div" disablePadding>
+                                <ListItem className={classes.nested}>
+                                    <Typography variant={"subtitle1"}>
+                                        {isEmail(data.title, data.description)}
+                                    </Typography>
+                                </ListItem>
+                            </List>
+                        </Collapse>
                     </Fragment>
                 )}
             </List>
