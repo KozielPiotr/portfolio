0e93f58e6108aa1c432d30c7d17d090a8c56e6e3
KozielPiotr <koziel.piotr1988@gmail.com>
2019-10-24 22:24:37 +0200

    Updates

---
+++ precache-manifest.b3cceecaf9eae73d318befd1e9502aa1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41888ba4cb171e665533686c573f0ab9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cf9eb4748d76e5da972a",
+    "url": "/portfolio/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "d54de5434523c327839b",
+    "url": "/portfolio/static/js/2.6b649211.chunk.js"
+  },
+  {
+    "revision": "cf9eb4748d76e5da972a",
+    "url": "/portfolio/static/js/main.9745ccc1.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  },
+  {
+    "revision": "8dbe73e475e788ed808b6a52a9005940",
+    "url": "/portfolio/static/media/hr-helper.8dbe73e4.jpg"
+  },
+  {
+    "revision": "cc0bf4027cd3030d01331ca25927c6e8",
+    "url": "/portfolio/static/media/portfolio.cc0bf402.jpeg"
+  },
+  {
+    "revision": "6c5ba13282d0cf695e0cd33434b4ee41",
+    "url": "/portfolio/static/media/schedules.6c5ba132.jpg"
+  },
+  {
+    "revision": "8ce1fd3d7f95c5606493dd527843e30d",
+    "url": "/portfolio/static/media/webCrawlerPrices.8ce1fd3d.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2aca319b805a9116a13dbba0454da080.js"
+  "/portfolio/precache-manifest.b3cceecaf9eae73d318befd1e9502aa1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
