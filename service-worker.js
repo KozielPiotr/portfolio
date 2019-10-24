66cba8be251426e8786ed6c1f5e9c0d8f1dd328e
KozielPiotr <koziel.piotr1988@gmail.com>
2019-10-24 19:24:35 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.9e61a58c431ef3100e300796e585458a.js"
+  "/portfolio/precache-manifest.b731841f7381201a3b21f8910263d79d.js"
 );
 
 self.addEventListener('message', (event) => {
